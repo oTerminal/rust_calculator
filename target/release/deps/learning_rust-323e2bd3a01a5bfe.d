bfb6eba1b402c1de0b95e4ad7f3b537f8abd3208
Vaibhav Prakash <pvaibhav2007@outlook.com>
2022-06-05 10:27:25 +0100

    Removed Target Folder

--- target/release/deps/learning_rust-323e2bd3a01a5bfe.d
+++
@@ -1,5 +0,0 @@
-/home/oterminal/Programming/learning_rust/target/release/deps/learning_rust-323e2bd3a01a5bfe: src/main.rs
-
-/home/oterminal/Programming/learning_rust/target/release/deps/learning_rust-323e2bd3a01a5bfe.d: src/main.rs
-
-src/main.rs:
--- target/release/learning_rust.d
+++
@@ -1,1 +0,0 @@
-/home/oterminal/Programming/learning_rust/target/release/learning_rust: /home/oterminal/Programming/learning_rust/src/main.rs
